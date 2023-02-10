34de1644fdd910dafee3eb4e2eb2111bd2badbbb
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-02-10 15:56:38 -0600

    Initial Commit

---
+++ kochSnowflake/kochSnowflakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  kochSnowflakeApp.swift
+//  kochSnowflake
+//
+//  Created by IIT PHYS 440 on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct kochSnowflakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
